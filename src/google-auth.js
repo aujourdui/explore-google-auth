2f22a30f02f6f347af953c14662e8b3d654d7526
ito <kyosuke.ito@gmail.com>
2022-04-26 13:11:28 -0700

    first commit

---
+++ src/google-auth.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+
+// function handleCredentialResponse(response) {
+//   console.log("Encoded JWT ID token: " + response.credential);
+// }
+// const Init = () => {
+//   google.accounts.id.initialize({
+//     client_id:
+//       "8981181688-431n7up5l1gbdl3ue17fhbsnv5pp4669.apps.googleusercontent.com",
+//     callback: handleCredentialResponse,
+//   });
+//   google.accounts.id.prompt();
+// };
+
+// export default Init;
